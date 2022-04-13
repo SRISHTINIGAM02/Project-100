b3f6ef487c643a291a4dff3de201c735e9b7abe4
SRISHTINIGAM02 <72446091+SRISHTINIGAM02@users.noreply.github.com>
2022-04-13 17:54:37 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,12 @@
+class atm(objects)
+
+def __init__(self,cardNumber, pinNumber):
+    self.cardNumber = cardNumber
+    self.pinNumber = pinNumber      
+
+def CashWithdrwal(self):
+    print("Cash Withdrawed")
+
+def BalanceEnquiry(self):
+    print("Enquired The Cash Balance")
+
